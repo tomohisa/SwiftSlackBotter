1c361cedf7d351a01e1b524597e163f30c6c12aa
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-04-06 13:48:27 +0900

    Issue #5893 fixing build issues

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftSlackBotter",
     dependencies: [
-    .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 3),
+      .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 3),
       .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 3),
     ]
 )
