9ae14ad872b233e7f561bf5aecb3d41d08cf8a21
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-04-06 18:05:45 +0900

    Issue #5893 update package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
       .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 3),
       .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 3),
+      .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
       .Package(url: "https://github.com/czechboy0/Environment.git", majorVersion: 0),
     ]
 )
