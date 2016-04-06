c4483802d7d83b2aaa5630cb137d0822647e1bc5
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-04-06 12:02:30 +0900

    Issue #5893 initial commit

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "SwiftSlackBotter"
+    dependencies: [
+      .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 4),
+      .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
+    ]
+)
