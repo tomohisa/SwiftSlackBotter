76be250fe92f10528fff03cf8b780b2302801a5a
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-04-06 12:07:10 +0900

    Issue #5893 Create Tag

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftSlackBotter"
+    name: "SwiftSlackBotter",
     dependencies: [
       .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 4),
       .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
