3235fdfa9b9f4865e69a749034e1ade6a95e19f1
Tomohisa Takaoka <tomohisa.takaoka@gmail.com>
2016-06-02 11:40:16 +0900

    add direct message user search (#11)

--- Sources/BotInfo.swift
+++ Sources/BotInfo.swift
@@ -23,95 +23,95 @@
 import JSON
 
 public struct BotInfo {
-  public enum ChannelType {
-    case Channel
-    case PrivateChannel
-    case DirectMessage
-  }
-  public let json : JSON?
-  public var users = [SlackUser]()
-  public var channels = [SlackChannel]()
-  public var privateChannels = [SlackGroup]()
-  public var directMessages = [SlackDirectMessage]()
-
-  public init(json:JSON? = nil) {
-    self.json = json
-    guard let json = json else {
-      return
+    public enum ChannelType {
+        case Channel
+        case PrivateChannel
+        case DirectMessage
     }
-    logger.debug(json)
-    if let users = json["users"]?.array {
-      for user in users {
-        self.users.append(SlackUser(json:user))
-      }
-    }
-    if let channels = json["channels"]?.array {
-      for channel in channels {
-        self.channels.append(SlackChannel(json:channel))
-      }
-    }
-    if let privateChannels = json["groups"]?.array {
-      for privateChannel in privateChannels {
-        self.privateChannels.append(SlackGroup(json:privateChannel))
-      }
-    }
-    if let directMessages = json["ims"]?.array {
-      for im in directMessages {
-        self.directMessages.append(SlackDirectMessage(json:im))
-      }
-    }
-  }
-  public var botId : String? {
-    get {
-      return self.json?["self"]?["id"]?.string
-    }
-  }
-  public func channelType(id:String?) -> ChannelType? {
-    guard let id = id else {
-      return nil
-    }
-    for channel in channels {
-      if channel.id == id && channel.is_channel {
-        return .Channel
-      }
-    }
-    for channel in privateChannels {
-      if channel.id == id && channel.is_group {
-        return .PrivateChannel
-      }
-    }
-    for channel in directMessages {
-      if channel.id == id && channel.is_im {
-        return .DirectMessage
-      }
-    }
-    return nil
-  }
-  public func channelIDFor(_ name:String?) -> String? {
-      guard let name = name else { return nil }
-      for channel in channels {
-        if channel.name == name && channel.is_channel {
-          return channel.id
+    public let json : JSON?
+    public var users = [SlackUser]()
+    public var channels = [SlackChannel]()
+    public var privateChannels = [SlackGroup]()
+    public var directMessages = [SlackDirectMessage]()
+    
+    public init(json:JSON? = nil) {
+        self.json = json
+        guard let json = json else {
+            return
         }
-      }
-      for channel in privateChannels {
-        if channel.name == name && channel.is_group {
-          return channel.id
+        logger.debug(json)
+        if let users = json["users"]?.array {
+            for user in users {
+                self.users.append(SlackUser(json:user))
+            }
         }
-      }
-      return nil
-  }
-  public func isChannel(id:String?) -> Bool {
-    guard let id = id else {
-      return false
+        if let channels = json["channels"]?.array {
+            for channel in channels {
+                self.channels.append(SlackChannel(json:channel))
+            }
+        }
+        if let privateChannels = json["groups"]?.array {
+            for privateChannel in privateChannels {
+                self.privateChannels.append(SlackGroup(json:privateChannel))
+            }
+        }
+        if let directMessages = json["ims"]?.array {
+            for im in directMessages {
+                self.directMessages.append(SlackDirectMessage(json:im))
+            }
+        }
     }
-    for channel in channels {
-      if channel.id == id && channel.is_channel {
-        return true
-      }
+    public var botId : String? {
+        get {
+            return self.json?["self"]?["id"]?.string
+        }
     }
-    return false
-  }
+    public func channelType(id:String?) -> ChannelType? {
+        guard let id = id else {
+            return nil
+        }
+        for channel in channels {
+            if channel.id == id && channel.is_channel {
+                return .Channel
+            }
+        }
+        for channel in privateChannels {
+            if channel.id == id && channel.is_group {
+                return .PrivateChannel
+            }
+        }
+        for channel in directMessages {
+            if channel.id == id && channel.is_im {
+                return .DirectMessage
+            }
+        }
+        return nil
+    }
+    public func channelIDFor(_ name:String?) -> String? {
+        guard let name = name else { return nil }
+        for channel in channels {
+            if channel.name == name && channel.is_channel {
+                return channel.id
+            }
+        }
+        for channel in privateChannels {
+            if channel.name == name && channel.is_group {
+                return channel.id
+            }
+        }
+        return nil
+    }
+    public func isChannel(id:String?) -> Bool {
+        guard let id = id else {
+            return false
+        }
+        for channel in channels {
+            if channel.id == id && channel.is_channel {
+                return true
+            }
+        }
+        return false
+    }
     public func userFor(id:String?) -> SlackUser? {
         guard let id = id else {
             return nil
@@ -123,57 +123,69 @@ public struct BotInfo {
         }
         return nil
     }
-  public func usernameFor(id:String?) -> String {
-    guard let id = id else {
-      return ""
+    public func usernameFor(id:String?) -> String {
+        guard let id = id else {
+            return ""
+        }
+        for user in users {
+            if user.id == id {
+                return user.name == nil ? "" : user.name!
+            }
+        }
+        return ""
     }
-    for user in users {
-      if user.id == id {
-        return user.name == nil ? "" : user.name!
-      }
+    public func userRealNameFor(id:String?) -> String {
+        guard let id = id else {
+            return ""
+        }
+        for user in users {
+            if user.id == id {
+                return user.real_name == nil ? "" : user.real_name!
+            }
+        }
+        return ""
     }
-    return ""
-  }
-  public func userRealNameFor(id:String?) -> String {
-    guard let id = id else {
-      return ""
+    public func userIdFor(username name:String?) -> String {
+        guard let name = name else {
+            return ""
+        }
+        for user in users {
+            if user.name == name {
+                if let userid = user.id {
+                    return userid
+                } else { return "" }
+            }
+        }
+        return ""
     }
-    for user in users {
-      if user.id == id {
-        return user.real_name == nil ? "" : user.real_name!
-      }
+    public func directMessageIdFor(userid id: String?) -> String {
+        guard let id = id else {
+            return ""
+        }
+        for directMessage in directMessages {
+            if directMessage.user == id {
+                guard let directMessageId = directMessage.id else { return "" }
+                return directMessageId
+            }
+        }
+        return ""
     }
-    return ""
-  }
-  public func userIdFor(username name:String?) -> String {
-    guard let name = name else {
-      return ""
+    public func directMessageIdFor(channelID: String?) -> String {
+        guard let id = channelID else {
+            return ""
+        }
+        for directMessage in directMessages {
+            if directMessage.id == id {
+                guard let userID = directMessage.user else { return "" }
+                return self.usernameFor(id: userID)
+            }
+        }
+        return ""
     }
-    for user in users {
-      if user.name == name {
-        if let userid = user.id {
-          return userid
-        } else { return "" }
-      }
+    public func directMessageIdFor(username name: String?) -> String {
+        guard let name = name else {
+            return ""
+        }
+        return directMessageIdFor(userid:userIdFor(username: name))
     }
-    return ""
-  }
-  public func directMessageIdFor(userid id: String?) -> String {
-    guard let id = id else {
-      return ""
-    }
-    for directMessage in directMessages {
-      if directMessage.user == id {
-        guard let directMessageId = directMessage.id else { return "" }
-        return directMessageId
-      }
-    }
-    return ""
-  }
-  public func directMessageIdFor(username name: String?) -> String {
-    guard let name = name else {
-      return ""
-    }
-    return directMessageIdFor(userid:userIdFor(username: name))
-  }
 }
